d4fe0e177585d29528742ebcdf74788b0c43240e
saibot0506 <tobias.backman@elev.ga.ntig.se>
2023-04-18 15:44:34 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello world!");
-    }
+    public static void main(String[] args) {
 
-    List<String> LoggFilen = new ArrayList<>();
+    }
 
     ArrayList<String> LoggFilen = readLogfile();
 
     private ArrayList<String> readLogfile() {
+
     }
 
 }
