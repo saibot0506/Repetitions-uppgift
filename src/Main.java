1a975177b742100cb1c80045eecc64fab250c48c
saibot0506 <tobias.backman@elev.ga.ntig.se>
2023-04-18 15:10:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {System.out.println("Hello world!");
+    }
+
+    List<String> LoggFilen = new ArrayList<>();
+
+    ArrayList<String> LoggFilen = readLogfile();
+
+    private ArrayList<String> readLogfile() {
+    }
+
+}
